488f90d9a46e355de066792c79e4552ed0608f3e
DominiqueCaron <dominique.caron@mail.mcgill.ca>
2025-07-30 11:23:52 -0700

    add disturbanceMeta

--- historicalDisturbances.R
+++ historicalDisturbances.R
@@ -21,12 +21,12 @@ defineModule(sim, list(
       "disturbanceSource", "character", "CanLaD", NA_character_, NA_character_, 
       desc = paste("The disturbance data source.", 
                    "Refer to data/availableData.csv to see which data is available.")
-      ),
+    ),
     defineParameter(
       "disturbanceYears", "numeric", NA_integer_, NA_integer_, NA_integer_, 
       desc = paste("The range of years for which disturbances are extracted.",
                    "Refer to data/availableData.csv to see which data is available.")
-      ),
+    ),
     defineParameter(
       "disturbanceTypes", "character", "wildfire", NA_character_, NA_character_, 
       desc = paste("The type of disturbance extracted.", 
@@ -43,15 +43,28 @@ defineModule(sim, list(
   ),
   inputObjects = bindrows(
     expectsInput(
+      objectName = "disturbanceMeta", objectClass = "data.table",
+      desc = paste(
+        "Table defining the disturbance event types in disturbanceRasters.",
+        "Need to be provided with disturbanceRasters."
+      ),
+      columns = c(
+        eventID             = "Event type ID",
+        name                = "Disturbance name (e.g. 'Wildfire').",
+        sourceValue         = "Value in `disturbanceRasters` to include as events",
+        sourceDelay         = "Optional. Delay (in years) of when the `disturbanceRasters` will take effect",
+      )
+    ),
+    expectsInput(
       objectName = "disturbanceRasters",
       objectClass = "list",
       desc = paste(
         "Set of spatial data sources containing locations of disturbance events for each year.",
-        "List items must be named by disturbance event types.",
+        "List items must be named by disturbance event type ID (e.g., \"1\").",
         "Within each event's list, items must be named by the 4 digit year the disturbances occured in.",
-        "For example, fires for 2025 can be accessed with `disturbanceRasters[[\"fires\"]][[\"2025\"]]`.",
+        "For example, fires for 2025 can be accessed with `disturbanceRasters[[\"1\"]][[\"2025\"]]`.",
         "Each disturbance item is a terra SpatRaster layer.",
-        "All non-NA areas will be considered events."
+        "Needs the input disturbanceMeta."
       )
     ), 
     expectsInput(objectName = "rasterToMatch", 
@@ -60,6 +73,17 @@ defineModule(sim, list(
                  sourceURL = NA)
   ),
   outputObjects = bindrows(
+    createsOutput(
+      objectName = "disturbanceMeta", objectClass = "data.table",
+      desc = "Table defining the disturbance event types.",
+      columns = c(
+        eventID             = "Event type ID",
+        name                = "Disturbance name (e.g. 'Wildfire'). Required only if 'disturbance_type_id' absent.",
+        sourceValue         = "Value in `disturbanceRasters` to include as events",
+        sourceDelay         = "Delay (in years) of when the `disturbanceRasters` will take effect",
+        sourceObjectName    = "Name of the object in the `simList` to retrieve the `disturbanceRasters` from annually."
+      )
+    ),
     createsOutput(objectName = "rstCurrentBurn", 
                   objectClass = "spatRaster", 
                   desc = "A binary raster with 1 values representing burned pixels."),
@@ -121,7 +145,17 @@ doEvent.historicalDisturbances = function(sim, eventTime, eventType) {
   #cacheTags <- c(currentModule(sim), "function:.inputObjects") ## uncomment this if Cache is being used
   dPath <- asPath(getOption("reproducible.destinationPath", dataPath(sim)), 1)
   message(currentModule(sim), ": using dataPath '", dPath, "'.")
+  
+  # Check that either disturbanceRasters and disturbanceMeta are provided, or neither
+  if(!suppliedElsewhere("disturbanceRasters", where = "user") & suppliedElsewhere("disturbanceMeta", where = "user")){
+    stop("Please provide meta data for the provided disturbanceRasters with the input disturbanceMeta.")
+  }
+  
+  if(suppliedElsewhere("disturbanceRasters", where = "user") & !suppliedElsewhere("disturbanceMeta", where = "user")){
+    stop("The disturbanceMeta should only be provided if the user provides disturbanceRasters.")
+  }
 
+  # Create disturbanceRasters and disturbanceMeta
   if (!suppliedElsewhere("disturbanceRasters")) {
 
     # By default, the module gets the disturbance for all simulation years, unless
@@ -132,7 +166,7 @@ doEvent.historicalDisturbances = function(sim, eventTime, eventType) {
       disturbanceYears <- P(sim)$disturbanceYears
     }
     
-    # Get all the rasters
+    # Get all the disturbance rasters
     sim$disturbanceRasters <- prepInputsDisturbances(
       source = P(sim)$disturbanceSource,
       types = P(sim)$disturbanceTypes,
@@ -140,8 +174,38 @@ doEvent.historicalDisturbances = function(sim, eventTime, eventType) {
       to = sim$rasterToMatch,
       destinationPath = inputPath(sim)
     ) |> Cache()
+    
+    sim$disturbanceMeta <- data.table(
+      eventID = c(),
+      name = c(),
+      sourceValue = c(),
+      sourceDelay = c(),
+      sourceObjectName = c()
+    )
+    if ("wildfire" %in% P(sim)$disturbanceTypes){
+      sim$disturbanceMeta <- rbind(sim$disturbanceMeta,
+                                   data.table(
+                                     eventID = 1L,
+                                     name = "Wildfire",
+                                     sourceValue = 1L,
+                                     sourceDelay = 0L,
+                                     sourceObjectName = "rstCurrentBurn"
+                                   )
+      )
+    }
+    if ("harvesting" %in% P(sim)$disturbanceTypes){
+      sim$disturbanceMeta <- rbind(sim$disturbanceMeta,
+                                   data.table(
+                                     eventID = 2L,
+                                     name = "Harvesting",
+                                     sourceValue = 1L,
+                                     sourceDelay = 0L,
+                                     sourceObjectName = "rstCurrentHarvest"
+                                   )
+      )
+    }
+    
   }
-  
   return(invisible(sim))
 }
 
